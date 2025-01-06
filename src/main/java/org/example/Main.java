21afa8d558a23592f541e70d10c10d6d2ff0676e
furkanozanyuce <furkanozanyuce@gmail.com>
2025-01-06 17:03:17 +0300

    Finished all tasks

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,25 +3,70 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(shouldWakeUp(true, 1));   // true
+        System.out.println(shouldWakeUp(false, 2));  // false
+        System.out.println(shouldWakeUp(true, 8));   // false
+        System.out.println(shouldWakeUp(true, -1));  // false
+
+        System.out.println("----------------------------------");
+
+        System.out.println(hasTeen(9, 99, 19)); // true
+        System.out.println(hasTeen(23, 15, 42)); // true
+        System.out.println(hasTeen(22, 23, 34)); // false
+
+        System.out.println("----------------------------------");
+
+        System.out.println(isCatPlaying(true, 10)); // false
+        System.out.println(isCatPlaying(false, 36)); // false
+        System.out.println(isCatPlaying(false, 35)); // true
+
+        System.out.println("----------------------------------");
+
+        System.out.println(area(5.0, 4.0)); // 20.0
+        System.out.println(area(-1.0, 4.0)); // -1
+
+        System.out.println("----------------------------------");
+
+        System.out.println(area(5.0)); //  78.53975
+        System.out.println(area(-1)); // -1
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if (clock < 0 || clock > 23) {
+            return false;
+        }
+        if (!isBarking) {
+            return false;
+        }
+        return (clock < 8 || clock >= 20);
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        if ((firstAge >=13 && firstAge <=19) || (secondAge >=13 && secondAge <=19) || (thirdAge >=13 && thirdAge <=19)) {
+            return true;
+        }
+        return false;
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        if (isSummer) {
+            return (temp >= 25 && temp <= 45);
+        } else {
+            return (temp >= 25 && temp <= 35);
+        }
     }
 
     public static double area(double width, double height) {
-       
+        if (width < 0 || height < 0) {
+            return -1;
+        }
+        return (width * height);
     }
 
     public static double area(double radius) {
-       
+        if (radius < 0) {
+            return -1;
+        }
+        return (radius * radius * Math.PI);
     }
 }
